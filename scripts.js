5112a5e8be04e846e5209f8ba6beda5c316d830f
huang-tiffany <huat2923@lgsstudent.org>
2021-07-16 17:35:45 -0700

    1st commit?

---
+++ scripts.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+
+//window.onload = () => {
+//	var navDiv = document.getElementById('nav-divider');
+//	if (navDiv.classList.indexOf(" exit") !== -1) {
+//		navDiv.classList.remove(" exit");
+//	}
+//}
+
+function leavePage(link) {
+	var navDiv = document.getElementById('nav-divider');
+	navDiv.className += " exit";
+	var vl = document.getElementById('vl');
+	vl.className += " exit";
+	var body = document.getElementById('body');
+	body.className += " fadeout";
+	setTimeout(() => {
+		if (link === "introduction") {
+			window.location.href = "introduction.html";
+		} else if (link === "resumes") {
+			window.location.href = "resumes.html";
+		} else if (link === "work") {
+			window.location.href = "work.html";
+		} else if (link === "play") {
+			window.location.href = "play.html";
+		} else if (link === "archive") {
+			window.location.href = "archive.html";
+		} else if (link === "home") {
+			window.location.href = "index.html";
+		} else {
+			location.reload();
+		}
+		console.log("hello");
+	}, 600);
+}
